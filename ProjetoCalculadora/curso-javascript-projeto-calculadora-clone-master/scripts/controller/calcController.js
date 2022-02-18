ae7a7f6226aef348a75587cb680533ac8a5de41a
xRy4n <zyrow.fs@gmail.com>
2022-02-17 21:32:20 -0300

    Adicionando botão porcento e igual

--- ProjetoCalculadora/curso-javascript-projeto-calculadora-clone-master/scripts/controller/calcController.js
+++ ProjetoCalculadora/curso-javascript-projeto-calculadora-clone-master/scripts/controller/calcController.js
@@ -1,6 +1,9 @@
 class CalcController{
 
     constructor(){
+        this._lastOperator = '';
+        this._lastNumber = '';
+
         this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
@@ -20,6 +23,8 @@ class CalcController{
         this.setdisplayDateTime();
         
     }, 1000);
+
+    this.setLastNumberToDisplay();
     
 
     /*setTimeout(()=>{
@@ -29,8 +34,6 @@ class CalcController{
 
         
     }
-
-
 
     setdisplayDateTime(){
         this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
@@ -52,11 +55,13 @@ class CalcController{
 
     clearAll(){
         this._operation = [];
+        this.setLastNumberToDisplay();
     }
 
     cancelEntry(){
         //metodo pop tira o ultimo item do array
         this._operation.pop();
+        this.setLastNumberToDisplay();
     }
 
     getLastOperation(){
@@ -83,16 +88,84 @@ class CalcController{
             this.calc();
         }
     }
+
+    getResult(){
+        return eval(this._operation.join(""));
+    }
+
     calc(){
 
-        let last = this._operation.pop();
+        let last = '';
 
-        let result = eval(this._operation.join(""));
+        this._lastOperator = this.getLastItem();
 
-        this._operation = [result, last];
+        if(this._operation.length < 3){
 
-        console.log(this._operation);
+            let firstItem = this._operation[0];
+            this._operation=[firstItem, this._lastOperator, this._lastNumber];
+        }
+        
+        if(this._operation.length > 3){
+            last = this._operation.pop();
+            this._lastNumber = this.getResult();
+        }
+
+        else if(this._operation.length == 3){
+
             
+            this._lastNumber = this.getLastItem(false);
+        }
+
+        let result = this.getResult();
+
+        if(last == '%'){
+
+            result /= 100;
+
+            this._operation = [result];
+
+        }else{
+
+            this._operation = [result];
+
+            if(last) this._operation.push(last);
+
+        }
+
+        this.setLastNumberToDisplay();
+            
+    }
+
+    getLastItem(isOperator = true){
+
+        let lastItem;
+
+        for(let i = this._operation.length-1; i >=0; i--){
+            if(this.isOperator(this._operation[i]) == isOperator){
+                lastItem = this._operation[i];
+                break;
+            }
+        }
+
+        if(!lastItem){
+
+            lastItem = (isOperator) ? this._lastOperator : this._lastNumber;
+        }
+
+        return lastItem;
+
+    }
+
+    setLastNumberToDisplay(){
+
+        let lastNumber = this.getLastItem(false);
+
+        if(!lastNumber) lastNumber = 0;
+
+        this.displayCalc = lastNumber;
+
+        
+
     }
 
     addOperation(value){
@@ -108,6 +181,7 @@ class CalcController{
             } else{
                 this.pushOperation(value);
 
+                this.setLastNumberToDisplay();
 
             }
             
@@ -120,6 +194,8 @@ class CalcController{
 
             let newValue =this.getLastOperation().toString() + value.toString();
             this.setLastOperation(parseInt(newValue));
+
+            this.setLastNumberToDisplay();
 
             }
             
@@ -167,7 +243,7 @@ class CalcController{
             break;
 
             case 'igual':
-               
+               this.calc();
             break;
 
             case 'ponto':
